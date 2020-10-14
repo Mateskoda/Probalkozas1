1949ae7077251185c3db7a3cc1bca3a97410c879
Mateskoda <skodamate93@gmail.com>
2020-10-14 08:35:51 +0200

    plus method findmin

--- src/ArrayNumMinMaxEven.java
+++ src/ArrayNumMinMaxEven.java
@@ -1,8 +1,18 @@
 public class ArrayNumMinMaxEven {
+
+private static final    int[] array1 = new int[20];
+
     public static void main(String[] args) {
-        int[] array1 = new int[20];
         for (int i = 0; i < array1.length; i++) {
             array1[i] = (int) (Math.random() * 101);
         }
+    }
+    public static int findMinOfArray (){
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i <array1.length ; i++) {
+            if (array1[i]<min){
+                min = array1[i];
+            }
+        }return min;
     }
 }
